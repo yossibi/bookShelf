1cf0fb572207dddd58a9c86d98e7b48e53b054b5
asafz83 <asafz83@gmail.com>
2014-10-23 19:02:43 +0300

    Create targil.js

---
+++ targil.js
@@ -0,0 +1,39 @@
+function Book (bookName, authorName, score) {
+	this.bookName = bookName;
+	this.authorName = authorName;
+	this.score = score;
+};
+
+
+function reset(){
+	document.getElementById('bookName').value = "";
+	document.getElementById('authorName').value = "";
+	document.getElementById('score').value = "";
+}
+
+function addBook(){
+	var bookName = document.getElementById('bookName').value;
+	var authorName = document.getElementById('authorName').value;
+	var score = document.getElementById('score').value;
+	var book = new Book(bookName, authorName, score);
+	addToList(book);
+	reset();
+}
+
+function addToList(book) {
+		var newElement = document.createElement("li");
+		var bookNameDiv = document.createElement("div");
+		bookNameDiv.innerHTML = book.bookName;
+		bookNameDiv.className = "left";
+		var authorNameDiv = document.createElement("div");
+		authorNameDiv.innerHTML = book.authorName;
+		authorNameDiv.className = "center";
+		var scoreDiv = document.createElement("div");
+		scoreDiv.innerHTML = book.score;
+		scoreDiv.className = "right";
+		newElement.appendChild(bookNameDiv);
+		newElement.appendChild(authorNameDiv);
+		newElement.appendChild(scoreDiv);
+		var ul = document.getElementById("bookList");
+		ul.appendChild(newElement);
+}
